2020e6055ab03e7cb6d165909f6b31922da63ed9
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-08-27 10:27:39 +0200

    Imagebox component

---
+++ src/components/ImgBox/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Image } from "./styles";
+
+const ImgBox = ({ x, y }) => {
+  console.log(x);
+  return <Image $initialX={x} $initialY={y} />;
+};
+
+export default ImgBox;
