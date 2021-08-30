fa57304a62fab9c6ebc06d4fcc826f1d62773a95
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-08-30 10:43:52 +0200

    Refactor calculateDistance function

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,10 @@ import React, { useState } from "react";
 import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
 import ImgBox from "./components/ImgBox";
-import { calculateDistance } from "./logic";
+import { calculateDistance, createMatrix } from "./logic";
 import { GlobalStyle, ImageContainer, Wrapper } from "./styles";
 
-// prettier-ignore
-const matrix = [
-    [0,0], [1,0], [2,0], [3,0],
-    [0,1], [1,1], [2,1], [3,1],
-    [0,2], [1,2], [2,2], [3,2],
-    [0,3], [1,3], [2,3], [3,3],
-    [0,4], [1,4], [2,4], [3,4],
-    [0,5], [1,5], [2,5], [3,5]
-
-];
+const matrix = createMatrix(4, 6);
 
 const App = () => {
   const [distance, setDistance] = useState(1);
--- src/logic.jsx
+++ src/logic.jsx
@@ -1,7 +1,20 @@
+//takes mouse X,Y as input, outputs distance from center from 0 (center) to 1 (max distance).
 export function calculateDistance([x, y]) {
   const center = [window.innerWidth / 2, window.innerHeight / 2];
   const maxHypot = Math.hypot(center[0], center[1]);
   const hypot = Math.hypot(center[0] - x, center[1] - y);
 
   return hypot / maxHypot;
+}
+
+export function createMatrix(x, y) {
+  const matrix = [];
+  matrix.length = x * y;
+
+  for (let i = 0; i < matrix.length; i++) {
+    const array = [Math.floor(i % x), Math.floor(i / x)];
+    matrix[i] = array;
+  }
+
+  return matrix;
 }
