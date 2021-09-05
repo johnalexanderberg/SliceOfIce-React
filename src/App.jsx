b5ce048e8d1fa77fddf6d6fd1323bd3fe941a53e
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-09-05 19:07:54 +0200

    re-slice on Click

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,9 @@ import ImgBox from "./components/ImgBox";
 import { calculateDistance, createMatrix } from "./logic";
 import { GlobalStyle, ImageContainer, Wrapper } from "./styles";
 
-const matrix = createMatrix(24, 36);
-const matrixDimensions = {
-  x: matrix[matrix.length - 1][0] + 1,
-  y: matrix[matrix.length - 1][1] + 1,
-};
-
 const App = () => {
   const [distance, setDistance] = useState(1);
+  const [matrix, setMatrix] = useState(createMatrix(8, 12));
 
   const easing = (num) => Math.pow(num, 3);
 
@@ -26,12 +21,27 @@ const App = () => {
     );
   };
 
+  const handleClick = () => {
+    const x = Math.floor(Math.random() * 11 + 1);
+    const y = Math.floor(Math.random() * 17 + 1);
+    setMatrix(createMatrix(x, y));
+  };
+
+  const matrixDimensions = {
+    x: matrix[matrix.length - 1][0] + 1,
+    y: matrix[matrix.length - 1][1] + 1,
+  };
+
   return (
     <div className="App">
       <GlobalStyle />
       <Header />
       <Footer />
-      <Wrapper onMouseMove={handleMove} onTouchMove={handleTouchMove}>
+      <Wrapper
+        onMouseMove={handleMove}
+        onTouchMove={handleTouchMove}
+        onClick={handleClick}
+      >
         <ImageContainer>
           {matrix.map(([x, y], index) => (
             <ImgBox
